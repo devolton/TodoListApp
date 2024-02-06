4a3ce0af76f740cda1b102d8e8fb28bd02a825e4
blirex <vanazubarev883@gmail.com>
2024-02-06 10:20:14 +0200

    Create class TodoDatabase and create function CreateTable and AddTask

---
+++ TodoDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace TodoList
+{
+    public class TodoDatabase
+    {
+        private static readonly string _databaseName = "TodoDatabase.db";
+        private static readonly SqliteConnection _connection;
+        private static readonly SqliteCommand _command;
+
+
+        static TodoDatabase()
+        {
+            _connection = new SqliteConnection("Data source = " + _databaseName);
+            _command = _connection.CreateCommand();
+
+            _connection.Open();
+
+            CreateTable();
+        }
+
+        public static void CreateTable()
+        {
+            _command.CommandText =
+                @"
+                CREATE TABLE IF NOT EXISTS TodoDatabase(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    descriptions TEXT NOT NULL,
+                    status BOOLEAN,
+                    deadlineDate TEXT
+                );
+            ";
+            _command.ExecuteNonQuery();
+        }
+    }
+}
